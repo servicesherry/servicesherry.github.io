15dc36954c70466f133c1ec21ebdbbbdf9a6a92f
servicesherry <service@sherrytek.com>
2017-03-21 16:43:06 +0800

    032121
    21

--- js/en_US.js
+++ js/en_US.js
@@ -90,8 +90,8 @@ var en_US={
     home_news:"News",
     event_header6:"Directors Appoint Announcement",
     event_header6_:"
-		<div class="news-info"><p><a href="http://www.neeq.com.cn/disclosure/2017/2017-02-07/1486453071_450956.pdf" target="_blank">Read More</a></div>
-",
+		<div class="news-info">
+		<p><a href="http://www.neeq.com.cn/disclosure/2017/2017-02-07/1486453071_450956.pdf" target="_blank">Read More</a></div>",
     event_header5:"The extraordinary shareholders' general meeting resolution notice for the first time",
     event_header5_:"http://www.neeq.com.cn/disclosure/2017/2017-02-07/1486453070_920199.pdf",
     event_header4:"Announcements of the extraordinary shareholders' general meeting for the first time",
